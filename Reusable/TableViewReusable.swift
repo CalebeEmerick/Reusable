d83be949c93c098ba6ee21a8b844eb4467387401
Calebe Emerick <calebe_emerick@hotmail.com>
2016-09-24 15:02:11 -0400

    TableViewReusable extension makes UITableView register Cell or Views easily.

--- Reusable/TableViewReusable.swift
+++ Reusable/TableViewReusable.swift
@@ -9,3 +9,41 @@
 import UIKit
 
 protocol TableViewReusable { }
+
+extension TableViewReusable where Self : UITableView {
+    
+    func register<T: UITableViewCell>(cellClass: T.Type) where T: Reusable {
+        
+        self.register(T.self, forCellReuseIdentifier: T.identifier)
+    }
+    
+    func register<T: UITableViewCell>(cellNib: T.Type) where T: Reusable {
+        
+        let nib = nibCreator(bundle: T.self)
+        
+        self.register(nib, forCellReuseIdentifier: T.identifier)
+    }
+    
+    func register<T: UIView>(viewClass: T.Type) where T: Reusable {
+        
+        self.register(T.self, forHeaderFooterViewReuseIdentifier: T.identifier)
+    }
+    
+    func register<T: UIView>(viewNib: T.Type) where T: Reusable {
+        
+        let nib = nibCreator(bundle: T.self)
+        
+        self.register(nib, forHeaderFooterViewReuseIdentifier: T.identifier)
+    }
+}
+
+extension TableViewReusable {
+    
+    fileprivate func nibCreator<T: UIView>(bundle: T.Type) -> UINib where T: Reusable {
+        
+        let bundle = Bundle(for: bundle.self)
+        let nib = UINib(nibName: T.identifier, bundle: bundle)
+        
+        return nib
+    }
+}
