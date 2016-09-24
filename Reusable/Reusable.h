dccca072115565f0f716d3aba23736773093fb58
Calebe Emerick <calebe_emerick@hotmail.com>
2016-09-24 12:28:54 -0400

    Initial Commit

---
+++ Reusable/Reusable.h
@@ -0,0 +1,19 @@
+//
+//  Reusable.h
+//  Reusable
+//
+//  Created by Calebe Emerick on 24/09/16.
+//  Copyright © 2016 CodeLLamas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reusable.
+FOUNDATION_EXPORT double ReusableVersionNumber;
+
+//! Project version string for Reusable.
+FOUNDATION_EXPORT const unsigned char ReusableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reusable/PublicHeader.h>
+
+
