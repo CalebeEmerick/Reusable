b6eb7946f929c26d98180cb20986f7faa370c40f
Calebe Emerick <calebe_emerick@hotmail.com>
2016-09-24 17:39:58 -0400

    CollectionViewReusable protocol.

---
+++ Reusable/CollectionViewReusable.swift
@@ -0,0 +1,11 @@
+//
+//  CollectionViewReusable.swift
+//  Reusable
+//
+//  Created by Calebe Emerick on 24/09/16.
+//  Copyright © 2016 CodeLLamas. All rights reserved.
+//
+
+import UIKit
+
+protocol CollectionViewReusable { }
