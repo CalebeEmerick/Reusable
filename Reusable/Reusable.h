f2806a466b95594458f7f5800dad2b233e48d509
Calebe Emerick <calebe_emerick@hotmail.com>
2016-09-24 13:32:27 -0400

    Project structure.

--- Reusable/Reusable.h
+++ Reusable/Reusable.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double ReusableVersionNumber;
 FOUNDATION_EXPORT const unsigned char ReusableVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Reusable/PublicHeader.h>
-
-
