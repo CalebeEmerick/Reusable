3e397945d768c963211561b6f9ce02f82da4b2c4
Calebe Emerick <calebe_emerick@hotmail.com>
2016-09-24 13:55:19 -0400

    Reusable protocol.

---
+++ Reusable/Reusable.swift
@@ -0,0 +1,14 @@
+//
+//  Reusable.swift
+//  Reusable
+//
+//  Created by Calebe Emerick on 24/09/16.
+//  Copyright © 2016 CodeLLamas. All rights reserved.
+//
+
+import UIKit
+
+protocol Reusable {
+    
+    static var identifier: String { get }
+}
