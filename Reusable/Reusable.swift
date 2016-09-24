503b8f9567924eabbe2a0b8c865823355bab6069
Calebe Emerick <calebe_emerick@hotmail.com>
2016-09-24 13:57:52 -0400

    Reusable extension makes any UIView has a unique identifier. That is his Class name.

--- Reusable/Reusable.swift
+++ Reusable/Reusable.swift
@@ -12,3 +12,11 @@ protocol Reusable {
     
     static var identifier: String { get }
 }
+
+extension Reusable where Self : UIView {
+    
+    static var identifier: String {
+        
+        return String(describing: self)
+    }
+}
